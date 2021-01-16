dd3ccb0d27063885b2ec822d03314c0517a6975d
i-Shanky <deep@ASUS-ROG.localdomain>
2021-01-16 19:35:02 +0530

    Created basic routing, validation for users

---
+++ backend/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const {signup} = require('../controllers/auth')
+const {runValidation} = require('../validators')
+const {userSignupValidator} = require('../validators/auth')
+
+router.post('/signup', userSignupValidator, runValidation, signup);
+module.exports = router;
