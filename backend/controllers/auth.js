e414047530ccb2ed571465ee7f56097bb8dfcfab
i-Shanky <deep@ASUS-ROG.localdomain>
2021-01-17 20:43:17 +0530

    Added sign-in feature

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const shortId = require('shortid')
-
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 exports.signup = (req,res) => {
     User.findOne({email: req.body.email}).exec((err, user)=> {
         if(user){
@@ -26,3 +27,44 @@ exports.signup = (req,res) => {
         })
     })
 }
+
+
+exports.signin = (req,res) => {
+    //check if user exists
+    const {email, password} = req.body
+    User.findOne({email}).exec((err,user)=> {
+        if(err || !user)
+            return res.status(400).json({
+                error: 'User with email doesnot exist! please signup'
+            });
+        //validate password
+        if(!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password donot match'
+            });
+        }
+        //generate json web token if successful 
+        const token = jwt.sign({_id: user._id},process.env.JWT_SECRET,{expiresIn: '1d'});
+        res.cookie('token',token, {expiresIn: '1d'})
+
+        const {_id,username, name, email, role} = user;
+        
+        res.status(200).json({
+            token,
+            user: {_id,username, name, email, role}
+        });
+    })
+
+}
+
+exports.signout = (req,res) => {
+    res.clearCookie("token")
+    res.json({
+        message: 'Signout successful'
+    });
+};
+//Check for token expiry using expressJwt
+exports.requireSignin =  expressJwt({
+    algorithms: ["HS256"],
+    secret: process.env.JWT_SECRET
+})
