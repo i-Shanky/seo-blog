4f891ace3875752cf10985b78ba7925d7a2e69b5
Shankhadeep Banerjee <shankha.m8@gmail.com>
2021-01-24 23:51:36 +0530

    Add frontend for tags and category

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -3,6 +3,7 @@ import {APP_NAME} from '../config';
 import Link from 'next/link'
 import Router from 'next/router';
 import {signout, isAuth} from '../actions/auth'
+import NProgress from 'nprogress'
 import {
   Collapse,
   Navbar,
@@ -14,6 +15,9 @@ import {
   NavbarText
 } from 'reactstrap';
 
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,9 +27,13 @@ const Header = (props) => {
   return (
     <div>
       <Navbar className="navbar navbar-light" style={{'backgroundColor': '#e3f2fd'}} expand="md" >
+        
+        <NavLink className="font-weight-bold" style={{ cursor: 'pointer'}}>
         <Link href="/">
-        <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
+        <img src="https://mdbootstrap.com/wp-content/uploads/2018/06/logo-mdb-jquery-small.png" alt="Title"></img>
         </Link>
+        </NavLink>
+        
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
@@ -49,20 +57,18 @@ const Header = (props) => {
           )}
           {isAuth() && (
               <React.Fragment>
-              <NavbarText>Hello {(isAuth().name).split(' ')[0]}!</NavbarText>
-              <NavItem>
+            <NavItem>
+                <Link href={isAuth().role!=1?'/user': '/admin'}>
+                <NavLink style={{ cursor: 'pointer'}}>
+                {`${(isAuth().name).split(' ')[0]}'s Dashboard`}
+                </NavLink>
+                </Link>
+            </NavItem>
+            <NavItem>
               <NavLink onClick={()=> signout(()=> Router.replace(`/login`))
               }>
                 <Link href="/login">
                   <a>Sign out</a>
-                </Link>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink onClick={()=>(isAuth().role!=1)?Router.replace(`/user`):Router.replace(`/admin`)
-              }>
-                <Link href="/user">
-                  <a>Dashboard</a>
                 </Link>
               </NavLink>
             </NavItem>
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,3 +1,4 @@
+import { link } from 'fs'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -8,6 +9,8 @@ class MyDocument extends Document {
             <meta charSet="UTF8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
+            <link rel="stylesheet" href="/static/css/styles.css"/>
         </Head>
         <body>
         <Main />
