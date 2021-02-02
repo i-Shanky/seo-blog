c19f5634409103411153e0b5e64fb519a327cdcd
Shankhadeep Banerjee <shankha.m8@gmail.com>
2021-02-02 20:33:15 +0530

    Add blog backend code

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {time} = require('../controllers/blog')
-
-router.get('/', time);
+const {create} = require('../controllers/blog')
+const {adminMiddleWare, requireSignin} = require('../controllers/auth')
+router.post('/blog', requireSignin, adminMiddleWare, create);
 module.exports = router;
